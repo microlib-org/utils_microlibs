0d9316a23eaeb0977a07ce9afb631c6ccbcce078
Hristo Vrigazov <hvrigazov@gmail.com>
2023-09-29 16:30:06 +0300

    Initialize library.

---
+++ socket_rpc/src/socket_rpc/standalone_server.py
@@ -0,0 +1,45 @@
+import pickle
+import logging
+import socket
+from typing import Callable
+
+
+def main(
+        callback: Callable,
+        host: str,
+        port: int,
+        buffer_size: int # buffer_size = 10 * 1024 * 1024 for 10MB
+):
+    # Define the server address and port
+    server_address = (host, port)
+
+    # Create and configure the socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind(server_address)
+        server_socket.listen()
+
+        logging.info(f"Server listening on {server_address}")
+
+        try:
+            while True:  # Infinite loop to continuously listen for connections
+                logging.info("Waiting for a connection...")
+                # Wait for a connection
+                connection, client_address = server_socket.accept()
+                with connection:
+                    logging.info('Connected by', client_address)
+                    # Receive the data
+                    data = b''
+                    while True:
+                        packet = connection.recv(buffer_size)
+                        if not packet: break
+                        data += packet
+
+                    # Deserialize the received data
+                    obj = pickle.loads(data)
+                    args = obj['args']
+                    kwargs = obj['kwargs']
+                    callback(*args, **kwargs)
+
+        except KeyboardInterrupt:
+            logging.info("Server is shutting down.")
+
