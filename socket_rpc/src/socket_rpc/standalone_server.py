3960eed92cb88fa2189b597af2f94f800915edde
Hristo Vrigazov <hvrigazov@gmail.com>
2023-10-04 10:07:56 +0300

    Refactor for a simple decorator.

--- socket_rpc/src/socket_rpc/standalone_server.py
+++ socket_rpc/src/socket_rpc/standalone_server.py
@@ -1,52 +1,47 @@
 import pickle
 import logging
 import socket
+from functools import wraps
 from typing import Callable
 
 
-def listen_callback(
-        callback: Callable,
-        host: str,
-        port: int,
-        buffer_size: int # buffer_size = 10 * 1024 * 1024 for 10MB
-):
-    # Define the server address and port
-    server_address = (host, port)
-
-    # Create and configure the socket
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.bind(server_address)
-        server_socket.listen()
-
-        logging.info(f"Server listening on {server_address}")
-
-        try:
-            while True:  # Infinite loop to continuously listen for connections
-                logging.info("Waiting for a connection...")
-                # Wait for a connection
-                connection, client_address = server_socket.accept()
-                with connection:
-                    logging.info(f'Connected by {client_address}')
-                    # Receive the data
-                    data = b''
-                    while True:
-                        packet = connection.recv(buffer_size)
-                        if not packet: break
-                        data += packet
-
-                    # Deserialize the received data
-                    obj = pickle.loads(data)
-                    args = obj['args']
-                    kwargs = obj['kwargs']
-                    callback(*args, **kwargs)
-
-        except KeyboardInterrupt:
-            logging.info("Server is shutting down.")
-
-
-def rpc(host: str, port: int, buffer_size: int):
+def rpc(host='127.0.0.1', port=5555, buffer_size=10*1024*1024):
     def decorator(callback: Callable):
-        def wrapper():
-            listen_callback(callback, host, port, buffer_size)
+        @wraps(callback)
+        def wrapper(*args, **kwargs):
+            # this is just to define the behavior when the decorated function is called directly
+            return callback(*args, **kwargs)
+
+        def listener():
+            server_address = (host, port)
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+                server_socket.bind(server_address)
+                server_socket.listen()
+
+                logging.info(f"Server listening on {server_address}")
+
+                try:
+                    while True:
+                        logging.info("Waiting for a connection...")
+                        connection, client_address = server_socket.accept()
+                        with connection:
+                            logging.info(f'Connected by {client_address}')
+                            data = b''
+                            while True:
+                                packet = connection.recv(buffer_size)
+                                if not packet: break
+                                data += packet
+
+                            obj = pickle.loads(data)
+                            callback_args = obj['args']
+                            callback_kwargs = obj['kwargs']
+                            callback(*callback_args, **callback_kwargs)
+
+                except KeyboardInterrupt:
+                    logging.info("Server is shutting down.")
+
+        # start the listener in the background when the function is decorated
+        listener()
+
         return wrapper
     return decorator
