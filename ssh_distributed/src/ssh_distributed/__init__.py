483b8cabcc746f0d669525dd77804130a5ee0c59
Hristo Vrigazov <hvrigazov@gmail.com>
2023-12-13 16:44:56 +0200

    Expose more functions.

--- ssh_distributed/src/ssh_distributed/__init__.py
+++ ssh_distributed/src/ssh_distributed/__init__.py
@@ -1,3 +1,10 @@
 __version__ = "0.1.0"
 
-from .base import connect_tunnel, upload_to_hosts
+from .base import (
+    connect_tunnel,
+    delete_directory_from_hosts,
+    download_from_hosts,
+    download_part_weights,
+    run_on_host,
+    upload_to_hosts
+)
