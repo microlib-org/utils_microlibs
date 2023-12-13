16f64de509aee87a8e94e4b7c348ba1b37fe0cad
Hristo Vrigazov <hvrigazov@gmail.com>
2023-12-13 09:24:06 +0200

    Wrapper of connect_tunnel in Python.

--- ssh_distributed/src/ssh_distributed/base.py
+++ ssh_distributed/src/ssh_distributed/base.py
@@ -1,18 +1,34 @@
+import logging
 import subprocess
 import os
 
 
-def call_connect_tunnel():
+def connect_tunnel(
+        from_host: str,
+        to_host: str,
+        from_port: int,
+        to_port: int,
+        tmux_session_name: str
+):
+    logging.info("Connect tunnel called. Make sure 'tmux' and 'autossh' are installed on all hosts.")
     # Determine the directory of base.py
     base_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the path to the connect_tunnel.sh script
     script_path = os.path.join(base_dir, 'connect_tunnel.sh')
 
-    # Call the script using subprocess
-    subprocess.run([script_path, "homebastion.local", "home02.local", "61002", "61000", "autossh_next"])
+    args = [
+        script_path, from_host, to_host, str(from_port), str(to_port), tmux_session_name
+    ]
+    subprocess.run(args)
 
 
 # Call the function
 if __name__ == "__main__":
-    call_connect_tunnel()
+    connect_tunnel(
+        from_host="homebastion.local",
+        to_host="home02.local",
+        from_port=61002,
+        to_port=61000,
+        tmux_session_name="autossh_next"
+    )
