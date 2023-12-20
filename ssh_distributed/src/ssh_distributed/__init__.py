166d9f87607c4af0f09d48b2c819301f514343bd
Hristo Vrigazov <hvrigazov@gmail.com>
2023-12-20 20:10:36 +0200

    Expose rsync file.

--- ssh_distributed/src/ssh_distributed/__init__.py
+++ ssh_distributed/src/ssh_distributed/__init__.py
@@ -1,10 +1,11 @@
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from .base import (
     create_tunnel,
     delete_directory_from_hosts,
     download_from_hosts,
     kill_tmux_session_on_host,
+    rsync_file,
     run_on_host,
     upload_to_hosts
 )
